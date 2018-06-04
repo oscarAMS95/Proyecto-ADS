71b823663f7dfedf0dbb8c8e0648d918d33f03a0
oscarAMS95 <32280104+oscarAMS95@users.noreply.github.com>
2018-06-03 22:13:40 -0700

    Add files via upload

---
+++ alertaContacto.js
@@ -0,0 +1,7 @@
+function msg1(){
+	alert("       Transaccion completada. \nEl donativo fue realizado con exito.    \n          Gracias por apoyar!\n");
+}
+
+function msg2(){
+	alert("                                    Aviso\nLa solicitud del material fue realizada con exito!\nLos datos del donativo son:\nNombre:\ntelefono:\nCorreo electronico:\n");
+}
